0ade250f679dfcea3719deec4eba1b5e159a6624
Faiyyaz25003 <khanwork25003@gmail.com>
2026-03-08 03:10:01 +0530

    library ka structure banaya hu

--- src/Components/LibraryManagement/PDFViewer.jsx
+++ src/Components/LibraryManagement/PDFViewer.jsx
@@ -1,7 +1,7 @@
 export default function PDFViewer({ pdf }) {
   return (
     <div className="fixed top-0 mt-[50px] left-0 w-full h-full bg-black bg-opacity-70 flex items-center justify-center">
-      <div className="bg-white w-[80%] h-[80%] rounded-lg p-4">
+      <div className="bg-white w-[100%] h-[100%] rounded-lg p-4">
         <iframe src={pdf} className="w-full h-full"></iframe>
       </div>
     </div>
